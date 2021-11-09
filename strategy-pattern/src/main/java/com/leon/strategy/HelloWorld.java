07bb0421e84ea3f04c17ede4bddf3fa63b937c7d
OceanLeonAI <beyondjayfir@126.com>
2021-11-09 15:12:05 +0800

    新分支推送

---
+++ strategy-pattern/src/main/java/com/leon/strategy/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.leon.strategy;
+
+/**
+ * @PROJECT_NAME: hello-design-pattern
+ * @CLASS_NAME: HelloWorld
+ * @AUTHOR: OceanLeonAI
+ * @CREATED_DATE: 2021/11/9 15:09
+ * @Version 1.0
+ * @DESCRIPTION:
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
